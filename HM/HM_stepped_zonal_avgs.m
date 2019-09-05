ca56dac2b61724e2f6bc63acafd679b177edf46f
fredglaw <Law@cims.nyu.edu>
2019-09-05 16:55:19 -0400

    debug and testing

--- HM/HM_stepped_zonal_avgs.m
+++ HM/HM_stepped_zonal_avgs.m
@@ -17,11 +17,11 @@ close all;
 L = 40; %full width of computational box;
 sc = L/(2*pi); %scaling factor to go from [-pi,pi] to [-L/2, L/2]
 N = 128; %number of nodes in each direction
-hype_visc = 5e-25; %hyperviscosity parameter, default 7e-23
+hype_visc = 7e-23; %hyperviscosity parameter, default 7e-23
 gamma = 8; %power on laplacian for hyperviscosity term
 kappa = 1; %mean density gradient
 alpha = 5; %adiabaticity parameter
-final_T = 1000;
+final_T = 1300;
 T = 5; %time increment to simulate with 
 N_time = T*200; %number of time steps
 dt = T/N_time;
@@ -49,7 +49,7 @@ init_freq = 1; init_T = 0;
 % init_q_h = reshape(fft2(init_q), [N*N,1]);
 
 % small in Fourier domain IC
-init_q_h = reshape(fft2(real(ifft2((1/500)*(rand(size(X))-(1/2))))), [N*N,1]);
+init_q_h = reshape(fft2(real(ifft2((1/500)*(rand(size(X))-(1/2))))), [N*N,1])/N;
 
 zero_mode = init_q_h(1); init_q_h = init_q_h(2:end); %keep zero mode separate
     
@@ -92,6 +92,8 @@ k_vals = (1/sc)*ifftshift(-ceil((N-1)/2):floor((N-1)/2)); %wavenumbers
 ZAMFTS = zeros(N,round(final_T/T)); %Zonally Averaged Mean Flow Time Series
 TKETS = zeros(1,round(final_T/T)); %Kinetic Energy Time Series (total)
 ZKETS = zeros(1,round(final_T/T)); %Kinetic Energy Time Series (zonal modes)
+TKETS_trap = zeros(1,round(final_T/T)); %Kinetic Energy Time Series (total), computed using trapezoidal
+ZKETS_trap = zeros(1,round(final_T/T)); %Kinetic Energy Time Series (zonal modes), computed using trapezoidal
 
 % init_phi_h = -reshape([zero_mode;init_q_h(:,end)],[N,N]) ./ (1 + (k_vals.^2) + ((k_vals').^2)); % get the vorticity
 
@@ -103,11 +105,11 @@ for i=1:round(final_T/T)
     if multistep_flag
         %AB2BDF2 solution
         [both_q_h,~] = ab2bdf2(init_q_h,T,N_time,noise_func,@HM_stiff,@HM_nonstiff,params_s,params_ns);
-        both_q_h = [zero_mode zero_mode; both_q_h]; q_h = reshape(both_q_h(:,end),[N,N]); q = ifft2(q_h); %put zero mode back
+        both_q_h = [zero_mode zero_mode; both_q_h]; q_h = reshape(both_q_h(:,end),[N,N]); q = ifft2(q_h)*N; %put zero mode back
     else
         %ETDRK4 solution
         [q_h,~] = etdrk(init_q_h,T,N_time,noise_func,@HM_stiff,@HM_nonstiff,params_s,params_ns);
-        q_h = [zero_mode; q_h]; q_h = reshape(q_h,[N,N]); q = ifft2(q_h); %put zero mode back in
+        q_h = [zero_mode; q_h]; q_h = reshape(q_h,[N,N]); q = ifft2(q_h)*N; %put zero mode back in
     end
     reshape(q_h,[N*N,1]); term_T = init_T + T;
     % disp(['Integrating forward by T=',num2str(T),' with M=',num2str(N_time),' steps took t=',num2str(t),' seconds']);
@@ -128,10 +130,10 @@ for i=1:round(final_T/T)
         if multistep_flag
     %         init_q_h = both_q_h(2:end,:);
              init_q_h = [reshape(fft2(real(ifft2(reshape(both_q_h(:,1),[N,N])))),[N*N,1]),...
-                 reshape(fft2(real(ifft2(reshape(both_q_h(:,2),[N,N])))),[N*N,1])];
+                 reshape(fft2(real(ifft2(reshape(both_q_h(:,2),[N,N])))),[N*N,1])]/N;
              init_q_h = init_q_h(2:end,:);
         else
-            init_q_h = reshape(fft2(real(q)),[N*N,1]);
+            init_q_h = reshape(fft2(real(q)),[N*N,1])/N;
             init_q_h = init_q_h(2:end);
         end
     else
@@ -141,6 +143,7 @@ for i=1:round(final_T/T)
     
 %     ZAMFTS(:,i) = get_zamf(phi_h,sc); %update ZAMFTS
     [TKETS(i), ZKETS(i)] = get_KE(phi_h,sc); %update TKETS,ZKETS
+    [TKETS_trap(i), ZKETS_trap(i)] = get_KE_trap(phi_h,sc); %update TKETS_trap,ZKETS_trap
 end
 t=toc;
 disp(['Integrating forward to T=',num2str(final_T),' with dt=',num2str(dt),' took t=',num2str(t),' seconds']);
@@ -164,7 +167,14 @@ colo = jet;
 figure(2);
 plot(linspace(T,final_T,round(final_T/T)),TKETS,'LineWidth',2); hold on;
 plot(linspace(T,final_T,round(final_T/T)),ZKETS,'LineWidth',2);
-legend('total kinetic energy', 'kinetic energy in zonal state')
+
+hold on;
+plot(linspace(T,final_T,round(final_T/T)),TKETS_trap,'LineWidth',2); hold on;
+plot(linspace(T,final_T,round(final_T/T)),ZKETS_trap,'LineWidth',2);
+
+% legend('total kinetic energy', 'kinetic energy in zonal state')
+legend('total kinetic energy', 'kinetic energy in zonal states', 'total kinetic energy,trap', 'kinetic energy in zonal states,trap')
+
 title('time series for kinetic energy')
 
 if saver_KE
@@ -181,26 +191,26 @@ function zamf = get_zamf(phi_h,sc)
 % potential in Fourier space.
 N = size(phi_h,1);
 k_vals = (1/sc)*ifftshift(-ceil((N-1)/2):floor((N-1)/2));
-zamf = (ifft(1i*k_vals.*phi_h(1,:))/N)';
+zamf = (ifft(1i*k_vals.*phi_h(1,:)))';
 % zamf = (ifft(phi_h(1,:))/N)';
 
 % top row of phi_h encodes the zonally averaged ES potential. We spectrally
 % differentiate, and divide by N to account for ifft instead of ifft2
 end
 
-% function [KE_tot,KE_zonal] = get_KE(phi_h,sc)
-% % Subroutine to integrate the kinetic energy of the current state given the
-% % current ES potential in Fourier space. 
-% N = size(phi_h,1);
-% k_vals = (1/sc)*ifftshift(-ceil((N-1)/2):floor((N-1)/2));
-% k_sq = k_vals.^2 + (k_vals.^2)';
-% E_vals = k_sq .* (abs(phi_h).^2);
-% KE_zonal = sum(E_vals(1,:));
-% KE_tot = sum(sum(E_vals));
-% end
+function [KE_tot,KE_zonal] = get_KE(phi_h,sc)
+% Subroutine to integrate the kinetic energy of the current state given the
+% current ES potential in Fourier space. 
+N = size(phi_h,1);
+k_vals = (1/sc)*ifftshift(-ceil((N-1)/2):floor((N-1)/2));
+k_sq = k_vals.^2 + (k_vals.^2)';
+E_vals = k_sq .* (abs(phi_h).^2);
+KE_zonal = sum(E_vals(1,:));
+KE_tot = sum(sum(E_vals));
+end
 
 %version which does in real space
-function [KE_tot,KE_zonal] = get_KE(phi_h,sc)
+function [KE_tot,KE_zonal] = get_KE_trap(phi_h,sc)
 % Subroutine to integrate the kinetic energy of the current state given the
 % current ES potential in Fourier space. 
 N = size(phi_h,1);
@@ -208,7 +218,7 @@ k_vals = (1/sc)*ifftshift(-ceil((N-1)/2):floor((N-1)/2));
 phi_y_h = (1i*phi_h.*(k_vals')); %row broadcasting for y-derivs
 phi_x_h = (1i*(k_vals .* phi_h)); %column broadcasting for x-derivs
 phi_x = ifft2(phi_x_h)*N; phi_y = ifft2(phi_y_h)*N;
-z_phi_x = ifft2(phi_x_h .* [1;zeros(N-1,1)])*N; z_phi_y = ifft2(phi_y_h.*[1;zeros(N-,1)])*N;
+z_phi_x = ifft2(phi_x_h .* [1;zeros(N-1,1)])*N; z_phi_y = ifft2(phi_y_h.*[1;zeros(N-1,1)])*N;
 E_vals = abs(phi_x).^2 + abs(phi_y).^2;
 z_E_vals = abs(z_phi_x).^2 + abs(z_phi_y).^2;
 KE_zonal = ((40/N)^2)*sum(sum(z_E_vals));
